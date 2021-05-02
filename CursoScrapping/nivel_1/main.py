eb562bdc11767d2b943e0a21c1e167ea3b0cb3de
Alexander96p <96xanderp@gmail.com>
2021-05-02 00:23:27 -0500

    configuraciones .ignore & nivel_1.1

---
+++ CursoScrapping/nivel_1/main.py
@@ -0,0 +1,30 @@
+import requests
+from lxml import html
+encabezado={
+    "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+}
+url = "https://www.wikipedia.org/"
+resp = requests.get(url,headers=encabezado)
+# print(resp.text)
+parser = html.fromstring(resp.text)
+# =====FORMA 1==========
+# contenido = parser.get_element_by_id("js-link-box-en")
+# print(contenido.text_content())
+# =====FORMA 2==========//valor exacto
+# contenido = parser.xpath("//a[@id='js-link-box-en']/strong/text()")
+# print(contenido)
+# ====== Forma 3 =======//valor similar
+# contenido=parser.xpath("//div[contains(@class,'central-featured-lang')]//strong/text()")
+# print(contenido)
+# for idioma in contenido:
+#     print(idioma)
+# ======= Forma 4 =====
+contenido=parser.find_class("central-featured-lang")
+for idioma in contenido:
+    print(idioma.text_content())
+
+# def print_hi(name):
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# if __name__ == '__main__':
+
+#     print_hi('PyCharm')
